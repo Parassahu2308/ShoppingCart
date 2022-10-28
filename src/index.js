0b478fcc6df0545ea24e53aae2fd344e71970981
Parassahu2308 <sahujuparas786@gmail.com>
2022-10-28 11:24:45 +0530

    Ui completed

--- src/index.js
+++ src/index.js
@@ -3,12 +3,15 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
+import store from "./Redux/store";
 
 // Redux
 import { Provider } from "react-redux";
 
 ReactDOM.render(
-    <App />,
+  <Provider store={store}>
+    <App />
+  </Provider>,
   document.getElementById("root")
 );
 
